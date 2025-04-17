b4e944268660697dc83e1ad0cd2a6391f2cbe454
sydneysantos12 <samuelsantos.sud07@gmail.com>
2025-04-17 17:00:08 -0300

    java e versão final

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function calcular(){
+    let valorGasolina = Number(document.getElementById("valorGasolina").value)
+    let valorAlcool = Number(document.getElementById("valorAlcool").value)
+
+    let resultado = ((valorAlcool * 70)/100)
+
+        if (resultado < valorGasolina){
+            document.getElementById("resultado").innerHTML = "compensa usar alcool"            
+        }
+        else if (resultado > valorGasolina){
+            document.getElementById("resultado").innerHTML = "compensa usar Gasolina"                                 
+        }
+        else if(resultado == valorGasolina){
+            document.getElementById("resultado").innerHTML = "erro"
+        }
+     
+}
